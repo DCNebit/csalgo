625b11875d7636c57b5ede277ded40b4fb0f0836
Darwin Loy C. Nebit <dcnebit@feu-eac.edu.ph>
2016-09-10 20:51:22 +0800

    I do edit pogi ako

--- gcd/src/gcd/Gcd.java
+++ gcd/src/gcd/Gcd.java
@@ -7,7 +7,7 @@ public class Gcd {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num1, num2, gcd;
-        System.out.print("Input number 1: ");
+        System.out.print("Pogi ako");
         num1 = input.nextInt();
         System.out.print("Input number 2: ");
         num2 = input.nextInt();
