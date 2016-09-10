772ebc2d3234a03eac754b99df8ef050a624b11c
Darwin Loy C. Nebit <dcnebit@feu-eac.edu.ph>
2016-09-10 20:47:43 +0800

    Adds GCD

---
+++ gcd/src/gcd/Gcd.java
@@ -0,0 +1,24 @@
+package gcd;
+
+import java.util.Scanner;
+
+public class Gcd {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2, gcd;
+        System.out.print("Input number 1: ");
+        num1 = input.nextInt();
+        System.out.print("Input number 2: ");
+        num2 = input.nextInt();
+
+        System.out.println(gcd(num1, num2));
+    }
+
+    static int gcd(int m, int n) {
+        if (n == 0) {
+            return m;
+        }
+        else return gcd(n, m%n);
+        }
+}
